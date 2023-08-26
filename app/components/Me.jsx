2789386d818f039faa6f82d5ab26975a703ba0bd
Brando <107069826+ColonelFPS@users.noreply.github.com>
2023-08-25 20:10:33 -0600

    Update Me.jsx

--- app/components/Me.jsx
+++ app/components/Me.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Image from 'next/image'
-import me from '../../public/Me.png'
+import me from '../../public/me.png'
 import ScrollWord from './ScrollWord'
 
 function Me() {
