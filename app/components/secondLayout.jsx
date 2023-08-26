f0eead87b713b2a618ea3b1ee3431be96c590d38
Brando <107069826+ColonelFPS@users.noreply.github.com>
2023-08-25 22:22:58 -0600

    Update secondLayout.jsx

--- app/components/secondLayout.jsx
+++ app/components/secondLayout.jsx
@@ -1,21 +1,11 @@
 import Navbar from './Navbar'
 import '../../app/globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Resplendant Designs',
-  description: 'Get rekt',
-}
 
 export default function SecondLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Navbar/>
-        </body>
-    </html>
+return (
+  <div>
+    {children}
+    <Navbar/>
+  </div>
   )
 }
